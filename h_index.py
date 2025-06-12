f8f06fce847dd6f828b2326a3f881ad57d16919a
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-11 23:33:44 -0400

    Second round

--- h_index.py
+++ h_index.py
@@ -1,9 +1,21 @@
 class Solution:
-    def hIndex(self, citations: List[int]) -> int:
+    def hIndex(self, citations: list[int]) -> int:
         citations.sort()
         hindex = len(citations)
         for i in range(len(citations)):
             if(citations[i] < hindex):
+                hindex -= 1
+            elif(citations[i] >= hindex):
+                break
+        return hindex
+
+
+class Solution:
+    def hIndex(self, citations: list[int]) -> int:
+        citations.sort()
+        hindex = len(citations)
+        for i in range(len(citations)):
+            if(hindex > citations[i]):
                 hindex -= 1
             elif(citations[i] >= hindex):
                 break
--- randomized_set.py
+++ randomized_set.py
@@ -31,6 +31,39 @@ class RandomizedSet:
         random.choice(self.data)
 
 
+
+
+
+class RandomizedSet:
+
+    def __init__(self):
+        self.data_map = {}
+        self.data = []        
+
+    def insert(self, val: int) -> bool:
+        if val in self.data_map : 
+            return False
+        
+        self.data_map[val] = len(self.data)
+        self.data.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        if val not in self.data_map : 
+            return False
+
+        val_pos = self.data_map[val]
+        self.data[val_pos] = self.data[-1]
+        self.data_map[self.data[-1]] = val_pos
+        
+        self.data.pop()
+        self.data_map.pop(val)
+
+        return True
+
+    def getRandom(self) -> int:
+        random.choice(self.data)
+
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
