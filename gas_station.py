b23012da60489bb74467420382c96d0df8f27f7d
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-13 23:18:16 -0400

    Second round

--- gas_station.py
+++ gas_station.py
@@ -16,3 +16,22 @@ class Solution:
             return -1
         else:
             return startidx
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        totaltank = 0
+        currenttank = 0
+        startidx = 0
+
+        for i in range(len(gas)):
+            totaltank += gas[i] - cost[i]
+            currenttank += gas[i] - cost[i]
+
+            if(currenttank < 0):
+                currenttank = 0
+                startidx = i + 1
+        
+        if totaltank < 0:
+            return -1
+        else:
+            return startidx
