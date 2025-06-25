01fc4e91e87842c40021349622fa49df0ec76155
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-25 18:35:20 -0400

    Second run

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -73,3 +73,31 @@ class Solution:
                 boxes[box_index].add(val)
 
         return True
+
+
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = [set() for _ in range(9)]
+        rows = [set() for _ in range(9)]
+        box  = [set() for _ in range(9)]
+
+        for i in range(9):
+            for j in range(9):
+                if board[i][j] == '.':
+                    continue
+                if board[i][j] in cols[i]:
+                    return False
+                cols[i].add(board[i][j])
+
+                if board[i][j] in rows[j]:
+                    return False 
+                rows[j].add(board[i][j])
+
+                box_idx = ((i//3) *3) + (j//3)
+
+                if board[i][j] in box[box_idx]:
+                    return False
+                box[box_idx].add(board[i][j])
+
+        return True
