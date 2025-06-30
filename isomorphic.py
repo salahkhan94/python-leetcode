05d98e146d272b068f5a4b8872f9290554cfebfe
Salahuddin Khan <salahuddin.khan.1994@gmail.com>
2025-06-29 23:51:06 -0400

    Second round

--- isomorphic.py
+++ isomorphic.py
@@ -12,3 +12,20 @@ class Solution:
             t_map[t_char] = s_char
         
         return True
+    
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_map = {}
+        t_map = {}
+
+        for s_char, t_char in zip(s,t) :
+            if (s_char in s_map and s_map[s_char] != t_char):
+                return False
+            elif(t_char in t_map and t_map[t_char] != s_char):
+                return False
+            
+            s_map[s_char] = t_char
+            t_map[t_char] = s_char
+        
+        return True
--- merge_intervals.py
+++ merge_intervals.py
@@ -37,4 +37,22 @@ class Solution:
         
         return merged
 
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        intervals.sort(key=lambda x: x[0])
+        merged = []
+        start = intervals[0][0]
+        end = intervals[0][1]
+
+        for interval in intervals:
+            cur_start = interval[0]
+            cur_end = interval[1]
+            if (cur_start > end) :
+                merged.append([start, end])
+                start = cur_start
+            end = max(end, cur_end)
+
+        
+        merged.append([start, end])
+        return merged
 
